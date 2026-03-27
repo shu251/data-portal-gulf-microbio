08b118996d80c7d4599787a9fac1918c6d59d10f
Sarah Hu <skhu@tamu.edu>
2026-03-27 16:29:07 -0500

    revisions

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# data-portal-gulf-microbio
+# Northern Gulf data portal
+
+Explore microbial eukaryotic biodiversity within the Northern Gulf.
+
+https://shu251.github.io/data-portal-gulf-microbio/
---
+++ app.R
@@ -0,0 +1,34 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    https://shiny.posit.co/
+#
+library(shinylive)
+library(httpuv)
+library(shiny)
+library(shiny)
+library(ggplot2)
+library(DT)
+
+ui <- fluidPage(
+  plotOutput(outputId = "plot",
+             brush = "plot_brush"),
+  DTOutput(outputId = "table")
+)
+
+server <- function(input, output) {
+  output$plot <- renderPlot(
+    ggplot(mtcars) +
+      geom_point(aes(x = mpg,
+                     y = disp))
+  )
+  output$table <- renderDT({
+    brushedPoints(df = mtcars,
+                  brush = input$plot_brush)
+  })
+}
+
+shinyApp(ui = ui, server = server)
